42f07b7c01476dcb948865861180e04b97f0fc68
DenysLapytskyi <94646847+DenysLapytskyi@users.noreply.github.com>
2022-06-19 22:17:42 +0300

    add all js and markup

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const createGalleryMarkup = createGalleryItemsMarkup(galleryItems)
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryMarkup)
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+   <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+            
+})
+.join('')
+
+}
+const onContainerClick = (e) => {
+    e.preventDefault();
+  if (e.target.classList.contains("gallery")) return;
+    const source = e.target.dataset.source;
+
+ const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+;
+
+
+}
+
+
+
+galleryContainer.addEventListener("click", onContainerClick);
